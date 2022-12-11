ac19156cd2f2ac326d4643bccdf53e57e67909f5
jakub.wszola <jakub.wszola@asseco.pl>
2022-12-11 17:10:04 +0100

    exercise

--- src/main/java/pl/teb/spring/Application.java
+++ src/main/java/pl/teb/spring/Application.java
@@ -9,4 +9,9 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+//    stwórz encje Car o polach: id, carMake, model, engine
+//    stwórz CarRepository
+//    stwórz klasę CarService, któa zapisze samochód
+//    stwórz CarController, który wystawi metody z serwisu
 }
