24e40ac44574b5a1536c143c3c45591af02ea117
jakub.wszola <jakub.wszola@asseco.pl>
2022-12-13 08:48:06 +0100

    Jpa example

--- src/main/java/pl/teb/spring/Application.java
+++ src/main/java/pl/teb/spring/Application.java
@@ -1,7 +1,18 @@
 package pl.teb.spring;
 
+import com.github.javafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import pl.teb.spring.infrastructure.entity.Person;
+import pl.teb.spring.infrastructure.repository.PersonRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -10,8 +21,28 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-//    stwórz encje Car o polach: id, carMake, model, engine
-//    stwórz CarRepository
-//    stwórz klasę CarService, któa zapisze samochód
-//    stwórz CarController, który wystawi metody z serwisu
+    @Bean
+    CommandLineRunner commandLineRunner(PersonRepository personRepository) {
+        Faker faker = new Faker();
+        Person person = new Person(faker.name().firstName(), faker.name().lastName(),
+                faker.number().numberBetween(17, 55));
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("firstname").descending());
+        return args -> {
+            personRepository.save(person);
+            personRepository.findById(1L).ifPresent(System.out::println);
+            personRepository.saveAll(generatePersonList());
+            Page<Person> page = personRepository.findAll(pageRequest);
+        };
+    }
+
+    private List<Person> generatePersonList() {
+        List<Person> personList = new ArrayList<>();
+        Faker faker = new Faker();
+
+        for (int i = 0; i < 100; i++) {
+            personList.add(new Person(faker.name().firstName(), faker.name().lastName(),
+                    faker.number().numberBetween(17, 55)));
+        }
+        return personList;
+    }
 }
