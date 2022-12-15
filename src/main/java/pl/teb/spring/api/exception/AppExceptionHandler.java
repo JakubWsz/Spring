8a81fecfdd21d118e115260cb59f266f6632a7cd
jakub.wszola <jakub.wszola@asseco.pl>
2022-12-15 15:02:57 +0100

    web validation

--- src/main/java/pl/teb/spring/api/exception/AppExceptionHandler.java
+++ src/main/java/pl/teb/spring/api/exception/AppExceptionHandler.java
@@ -1,13 +1,21 @@
 package pl.teb.spring.api.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import pl.teb.spring.domain.exception.DomainException;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Slf4j
@@ -19,32 +27,38 @@ public class AppExceptionHandler {
         log.error("Domain exception occurred", exception);
 
         HttpStatus httpStatus = HttpStatus.valueOf(exception.getCode().getStatus());
-        Error body = new Error(httpStatus.value(), exception.getMessage(), Instant.now());
+        Error body = new Error(httpStatus.value(), exception.getMessage());
 
         return new ResponseEntity<>(body, httpStatus);
     }
 
-    public static class Error {
-        private final int status;
-        private final String message;
-        private final Instant timestamp;
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Error> defaultHandler(MethodArgumentNotValidException e) {
+        return new ResponseEntity<>(new Error(400,
+                "Request validation failed",
+                e.getBindingResult().getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.toList())),
+                HttpStatus.BAD_REQUEST);
+    }
 
-        private Error(int status, String message, Instant timestamp) {
+    @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+    @Getter
+    public static class Error {
+        int status;
+        String message;
+        @JsonInclude(JsonInclude.Include.NON_EMPTY)
+        List<String> details;
+        Instant timestamp = Instant.now();
+
+        public Error(int status, String message) {
             this.status = status;
             this.message = message;
-            this.timestamp = timestamp;
+            details = null;
         }
 
-        public int getStatus() {
-            return status;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public Instant getTimestamp() {
-            return timestamp;
+        public Error(int status, String message, List<String> details) {
+            this.status = status;
+            this.message = message;
+            this.details = details;
         }
     }
 }
